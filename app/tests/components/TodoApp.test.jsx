0ece7fd2416ff73bdf411469ae8b3f4c36775de2
Admin <andre@vitorio.net>
2017-04-20 11:39:22 +0200

    Create AddTodo component

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,6 +1,6 @@
-var React = ('react');
-var ReactDOM = ('react-dom');
-var TestUtils = ('react-addons-test-utils');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
 
