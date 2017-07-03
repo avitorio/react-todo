6b1c8bc146ed849a96a75c0fce7ad4c13e78af4a
Andre <andre@vitorio.net>
2017-07-03 10:44:39 +0200

    Refactor TodoList, Todo and AddTodo to work with Redux

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,18 +1,13 @@
-var redux = require('redux');
-var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
-
-export var configure = () => {
-	var reducer = redux.combineReducers({
-		searchText: searchTextReducer,
-		showCompleted: showCompletedReducer,
-		todos: todosReducer
-	});
-
-	var store = redux.createStore(
-		reducer, 
-		redux.compose(
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-	 );
-
-	return store;
+import { combineReducers, createStore, compose } from 'redux';
+import { searchTextReducer, showCompletedReducer, todosReducer } from 'reducers';
+ 
+export const configure = (initialState = {}) => {
+    const reducer = combineReducers({
+        searchText: searchTextReducer,
+        showCompleted: showCompletedReducer,
+        todos: todosReducer
+    });
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(reducer, initialState, composeEnhancers());
+    return store;
 };
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -4,7 +4,7 @@ var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
 
-var Todo = require('Todo');
+var {Todo} = require('Todo');
 
 
 describe('Todo', () => {
@@ -12,7 +12,7 @@ describe('Todo', () => {
 		expect(Todo).toExist();
 	});
 
-	it('Should call onToggle prop with id on click', () => {
+	it('should dispatch TOGGLE_TODO action on click', () => {
 		var todoData = {
 			id: 199,
 			text: 'Write Todo.jsx.test test',
@@ -21,12 +21,15 @@ describe('Todo', () => {
 
 		var spy = expect.createSpy();
 
-		var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy} />);
+		var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy} />);
 
 		var $el = $(ReactDOM.findDOMNode(todo));
 
 		TestUtils.Simulate.click($el[0]);
 
-		expect(spy).toHaveBeenCalledWith(199);
+		expect(spy).toHaveBeenCalledWith({
+			type: 'TOGGLE_TODO',
+			id: todoData.id
+		});
 	});
 });
